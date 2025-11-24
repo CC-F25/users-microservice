fdd89ac240506f5b066bbe5638561b71b20595c8
Amaan Sheikh <100550299+amaan784@users.noreply.github.com>
2025-11-23 21:29:29 -0500

    updated database_connection.py to passwords with special chars

--- database_connection.py
+++ database_connection.py
@@ -1,12 +1,16 @@
 import os
+import urllib.parse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Define constants for connection details
 db_user = os.environ.get("MYSQL_USER", "root")
-db_pass = os.environ.get("MYSQL_PASSWORD", "your_password")
+db_pass_raw = os.environ.get("MYSQL_PASSWORD", "your_password")
 db_name = os.environ.get("MYSQL_DB", "users_db")
+
+# ENCODE THE PASSWORD to handle special chars like @, #, /
+db_pass = urllib.parse.quote_plus(db_pass_raw)
 
 # check for Cloud SQL Connection Name
 cloud_sql_connection_name = os.environ.get("INSTANCE_CONNECTION_NAME")
